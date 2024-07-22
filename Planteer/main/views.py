0da8776770d9390432d574b19039ad90dcfad432
Sarah <monifsarah@gmail.com>
2024-07-22 14:03:42 +0300

    Limited results in the home page to 3 plants

--- Planteer/main/views.py
+++ Planteer/main/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
-# from games.models import Game
+from plants.models import Plant
 
 # Create your views here.
 
 def home_view(request:HttpRequest):
+    plants=Plant.objects.all()[0:3]
 
-    #get all games
-
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html',{"plants":plants})
