e20460b588402bff9ea04acee97e46f8b9339d3e
Sarah <monifsarah@gmail.com>
2024-07-22 11:41:27 +0300

    added contact us page

--- Planteer/contact/urls.py
+++ Planteer/contact/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "contact"
 
 urlpatterns = [
-    path('contact/', views.contact_view, name="contact_view"),
+    path('', views.contact_view, name="contact_view"),
     # path('contact/messages', views.contact_messages, name="contact_messages"),
 
 ]
--- Planteer/contact/views.py
+++ Planteer/contact/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from contact.models import Contact
 
 # Create your views here.
 
 def contact_view(request:HttpRequest):
-    print("initial contact")
+    if request.method == "POST":
+        new_contact=Contact(
+            first_name=request.POST["first_name"],
+            last_name=request.POST["last_name"],
+            email=request.POST["email"],
+            message=request.POST["message"],
+        )
+        new_contact.save()
+        return redirect('main:home_view')
+    return render(request, "contact/contact_view.html")
 
 # def contact_messages
+    
