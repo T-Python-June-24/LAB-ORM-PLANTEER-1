0c95ebd1b982e023e414504a9e9e06955982a9fd
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-07-23 09:12:27 +0300

    Uncommented views

--- Planteer/main/views.py
+++ Planteer/main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-# from plants import Plant
+from plants import Plant
 
 
 
 # Create your views here.
 def index(request):
-    # plants = Plant.objects.all()
-    # return render(request, 'main/index.html', {'plants': plants})
-    return render(request, 'main/index.html')
+    plants = Plant.objects.all()
+    return render(request, 'main/index.html', {'plants': plants})
+    # return render(request, 'main/index.html')
